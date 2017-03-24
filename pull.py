c3d100a995b8a3c53945b58425ad39bcbedcdb41
Chris McKenzie <kristopolous@yahoo.com>
2017-03-24 12:55:41 -0700

    removing the verbose output of the pull script

--- pull.py
+++ pull.py
@@ -21,7 +21,6 @@ def stub(what):
 def get(url):
     return requests.get( url, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0'} ).content
 
-print(now)
 with open('sitelist.json', 'r') as sitelist:
     siteMap = json.load(sitelist)
     for site in siteMap:
@@ -37,7 +36,6 @@ with open('sitelist.json', 'r') as sitelist:
 
             url = site[key]
             try:
-                print("%s: %s" %(site['name'], url) )
                 data = get( url )
                 path = "%s%s.%s" % (basepath, stub( site['name'] ), ext)
 
